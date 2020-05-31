8af171499cc33900f744018f1b2ce9249adceaef
Shani Zlotnik <shanizlotnik@gmail.com>
2020-05-31 21:25:16 +0300

    added tasks interface and tasks status

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private TasksService: TasksService) {}
     
     @Get()
-    getAllTasks() {
-        this.TasksService.getAllTasks();
+    getAllTasks(): Task[] {
+        return this.TasksService.getAllTasks();
     }
 }
