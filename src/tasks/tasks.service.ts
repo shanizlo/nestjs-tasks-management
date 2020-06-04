69fda7eac88fc56b47ac9d8a12102704a5c52277
Shani Zlotnik <shanizlotnik@gmail.com>
2020-06-04 18:27:31 +0300

    refactored getTasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -13,26 +13,9 @@ export class TasksService {
         private taskRepository: TaskRepository,
     ) {}
 
-    // getAllTasks(): Task[] {
-    //     return this.tasks;
-    // }
-
-    // getTasksWithFilters(filterDto: GetTasksFilterDto) {
-    //     const { status, search } = filterDto;
-
-    //     let tasks = this.getAllTasks();
-    //     if (status) {
-    //         tasks = tasks.filter(task => task.status === status);
-    //     }
-
-    //     if (search) {
-    //         tasks = tasks.filter(task => 
-    //             task.title.includes(search) ||
-    //             task.description.includes(search))
-    //     }
-
-    //     return tasks;
-    // }
+    async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+        return this.taskRepository.getTasks(filterDto);
+    }
 
     async getTaskById(id: number): Promise<Task> {
         const found =  await this.taskRepository.findOne(id);
@@ -48,11 +31,12 @@ export class TasksService {
 
     }
 
-    // updateTaskStatus(id: string, status: TaskStatus): Task {
-    //     const task = this.getTaskByID(id);
-    //     task.status = status;
-    //     return task;
-    // }
+    async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
+        const task = await this.getTaskById(id);
+        task.status = status;
+        await task.save();
+        return task;
+    }
 
     async deleteTask(id: number): Promise<void> {
         const result = await this.taskRepository.delete(id);
