869c933b35e658f627ac562951c17018a4f8fb0a
Shani Zlotnik <shanizlotnik@gmail.com>
2020-06-05 15:11:30 +0300

    added auth

---
+++ src/auth/user.entity.ts
@@ -0,0 +1,12 @@
+import { BaseEntity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+export class User extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    username: string;
+
+    @Column()
+    password: string;
+}
