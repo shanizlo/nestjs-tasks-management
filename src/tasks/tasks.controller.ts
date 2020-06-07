17d0252ca12f3fb2a5da8fab16bb80d966d99f6d
Shani Zlotnik <shanizlotnik@gmail.com>
2020-06-07 21:52:25 +0300

    create task for user

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Get, Post, Body, Param, Delete, Put, Patch, Query, UsePipes, ValidationPipe, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Put, Patch, Query, UsePipes, ValidationPipe, ParseIntPipe, UseGuards } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { response } from 'express';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 import { Task } from './task.entity';
 import { TaskStatus } from './task-status.enum';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from '../auth/get-user.decorator';
+import { User } from 'src/auth/user.entity';
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
     constructor(private tasksService: TasksService) {}
     
@@ -23,8 +26,11 @@ export class TasksController {
 
     @Post()
     @UsePipes(ValidationPipe)
-    createTask(@Body() createTaskDTO: CreateTaskDto): Promise<Task> {
-        return this.tasksService.createTask(createTaskDTO);
+    createTask(
+        @Body() createTaskDTO: CreateTaskDto,
+        @GetUser() user: User
+        ): Promise<Task> {
+        return this.tasksService.createTask(createTaskDTO, user);
 
     }
 
